99ea581213d61fe9254e1a343f8d292a9990edae
Forpee <khalilhassam3@gmail.com>
2022-06-07 19:30:36 +0200

    a

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -5,6 +5,6 @@ varying vec3 vNormal;
 
 void main()
 {
-    float diff=dot(vec3(1.),normalize(vNormal));
-    gl_FragColor=vec4(vec3(diff),1.);
+    float diff=dot(normalize(vec3(1.)),vNormal);
+    gl_FragColor=vec4(vec3(abs(diff)),1.);
 }
